6146bbd11ddc0649bb2acd981de5677840c02d89
VILOZNY <efrat.vilozny@sap.com>
2016-07-03 14:54:09 +0300

    hrf poc

---
+++ examples/grammars/hrf/hrf_json.js
@@ -0,0 +1,199 @@
+
+
+(function(root, factory) {
+    if (typeof module === 'object' && module.exports) {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory(require('chevrotain'));
+    } else {
+        // Browser globals (root is window)
+        root["JsonParser"] = factory(root.chevrotain).JsonParser;
+    }
+}(this, function(chevrotain) {
+// ----------------- lexer -----------------
+    var extendToken = chevrotain.extendToken;
+    var Lexer = chevrotain.Lexer;
+    var Parser = chevrotain.Parser;
+
+    var Identifier = extendToken("Identifier", /[a-zA-z]\w+/);
+
+var dynamicTokens = [{name: "AttrStrTerm", regex: /name of the player/}, {name: "AttrNumberTerm", regex: /age of the player/}];
+
+    // tbd - loop on array and prepare array of extendToken, push to allTokens order improtand 
+
+
+// In ES6, custom inheritance implementation (such as 'extendToken(...)') can be replaced with simple "class X extends Y"...
+var True = extendToken("True", /true/);
+var False = extendToken("False", /false/);
+var Null = extendToken("Null", /null/);
+var IsEqual = extendToken("IsEqual", /is equal to/);
+var IsNotEqual = extendToken("IsNotEqual", /is not equal to/);
+/*var LCurly = extendToken("LCurly", /{/);
+var RCurly = extendToken("RCurly", /}/);
+var LSquare = extendToken("LSquare", /\[/);
+var RSquare = extendToken("RSquare", /]/);*/
+var Comma = extendToken("Comma", /,/);
+var Colon = extendToken("Colon", /:/);
+var StringLiteral = extendToken("StringLiteral", /'(?:[^\\']+|\\(?:[bfnrtv'\\/]|u[0-9a-fA-F]{4}))*'/);
+//var StringLiteral = extendToken("StringLiteral", /"(?:[^\\"]+|\\(?:[bfnrtv"\\/]|u[0-9a-fA-F]{4}))*"/);
+var NumberLiteral = extendToken("NumberLiteral", /-?(0|[1-9]\d*)(\.\d+)?([eE][+-]?\d+)?/);
+var WhiteSpace = extendToken("WhiteSpace", /\s+/);
+WhiteSpace.GROUP = Lexer.SKIPPED; // marking WhiteSpace as 'SKIPPED' makes the lexer skip it.
+
+    var allTokens = []//[NumberLiteral, StringLiteral,  Comma, Colon, True, False, Null, IsEqual, IsNotEqual];
+
+
+    var idx;
+
+
+    for (idx = 0; idx < dynamicTokens.length; idx++) {
+        allTokens.push(extendToken(dynamicTokens[idx].name, dynamicTokens[idx].regex, Identifier));
+    }
+    allTokens.push.apply(allTokens, [NumberLiteral, StringLiteral,  Comma, Colon, True, False, Null, IsEqual, IsNotEqual]); //apply();
+    allTokens.push(WhiteSpace);
+    allTokens.push(Identifier);
+
+    var JsonLexer = new Lexer(allTokens);
+
+
+
+
+// ----------------- parser -----------------
+
+function JsonParser(input) {
+    // invoke super constructor
+    Parser.call(this, input, allTokens, {
+        // by default the error recovery / fault tolerance capabilities are disabled
+        // use this flag to enable them
+        recoveryEnabled: true}
+    );
+
+    // not mandatory, using <$> (or any other sign) to reduce verbosity (this. this. this. this. .......)
+    var $ = this;
+
+    this.singleStatementBoolean = this.RULE("singleStatementBoolean", function() {
+        // @formatter:off
+        $.SUBRULE($.ruleValueClause);
+        $.SUBRULE($.ruleComparisonOption);
+        $.SUBRULE2($.ruleValueClause);
+        // @formatter:on
+    });
+
+    this.ruleComparisonOption = this.RULE("ruleComparisonOption", function() {
+        $.OR([
+            { ALT: function () { $.CONSUME(IsEqual) }},
+            { ALT: function () { $.CONSUME(IsNotEqual) }}
+
+        ]);
+
+    });
+
+
+    this.ruleValueClause = this.RULE("ruleValueClause", function() {
+        //$.CONSUME(NumberLiteral);
+        $.OR([
+            { ALT: function () { $.CONSUME(Identifier) }},
+            { ALT: function () { $.CONSUME(NumberLiteral) }},
+            { ALT: function () { $.CONSUME(StringLiteral) }}
+
+        ]);
+
+
+    });
+
+    /*this.singleStatementBoolean = this.RULE("singleStatementBoolean", function() {
+        // @formatter:off
+        $.OR([
+            { ALT: function () { $.SUBRULE($.ruleNumericSingleStatementComparison) }},
+            { ALT: function () { $.SUBRULE($.ruleStringSingleStatementComparison) }}
+        ]);
+        // @formatter:on
+    });*/
+
+   /* this.ruleNumericSingleStatementComparison = this.RULE("ruleNumericSingleStatementComparison", function() {
+        $.SUBRULE($.ruleNumericClause);
+        $.SUBRULE($.ruleNumericComparisonOption);
+        $.SUBRULE2($.ruleNumericClause);
+
+    });*/
+
+
+
+   /* this.ruleNumericClause = this.RULE("ruleNumericClause", function() {
+        //$.CONSUME(NumberLiteral);
+        $.OR([
+            { ALT: function () { $.CONSUME(Identifier) }},
+            { ALT: function () { $.CONSUME(NumberLiteral) }}
+
+        ]);
+
+
+    });*/
+
+
+
+    /*this.ruleNumericComparisonOption = this.RULE("ruleNumericComparisonOption", function() {
+        $.OR([
+            { ALT: function () { $.CONSUME(IsEqual) }},
+            { ALT: function () { $.CONSUME(IsNotEqual) }}
+
+        ]);
+
+    });
+
+    this.ruleStringSingleStatementComparison = this.RULE("ruleStringSingleStatementComparison", function() {
+        $.SUBRULE($.ruleStringClause);
+        $.SUBRULE($.ruleNumericComparisonOption);
+        $.SUBRULE2($.ruleStringClause);
+
+    });*/
+
+    /*this.ruleStringClause = this.RULE("ruleStringClause", function() {
+        //$.CONSUME(StringLiteral);
+        $.OR([
+            { ALT: function () { $.CONSUME(Identifier) }},
+            { ALT: function () { $.CONSUME(StringLiteral) }}
+
+        ]);
+
+    });*/
+
+
+
+    // @formatter:on
+
+    // very important to call this after all the rules have been defined.
+    // otherwise the parser may not work correctly as it will lack information
+    // derived during the self analysis phase.
+    Parser.performSelfAnalysis(this);
+}
+
+// inheritance as implemented in javascript in the previous decade... :(
+JsonParser.prototype = Object.create(Parser.prototype);
+JsonParser.prototype.constructor = JsonParser;
+
+// ----------------- wrapping it all together -----------------
+
+    return {
+
+        parseJson: function(text) {
+            var fullResult = {};
+            var lexResult = JsonLexer.tokenize(text);
+            fullResult.tokens = lexResult.tokens;
+            fullResult.ignored = lexResult.ignored;
+            fullResult.lexErrors = lexResult.errors;
+
+            var parser = new JsonParser(lexResult.tokens);
+            parser.singleStatementBoolean();
+            fullResult.parseErrors = parser.errors;
+
+            if (fullResult.lexErrors.length >= 1 || fullResult.parseErrors.length >= 1) {
+                throw new Error("sad sad panda")
+            }
+            return fullResult;
+        },
+
+        JsonParser: JsonParser
+    }
+}));
---
+++ examples/grammars/hrf/hrf_json_spec.js
@@ -0,0 +1,68 @@
+var assert = require("assert");
+var parseJson = require("./hrf_json").parseJson;
+
+
+describe('HRF JSON Grammar', function() {
+
+    it('HRF - can parse a simple hrf expression without errors', function() {
+        //var inputText = '5 is equal to 3';
+//        var lexAndParseResult = parseJson(inputText);
+//        var lexAndParseResult = parseJson(inputText);
+//        assert.equal(lexAndParseResult.lexErrors.length, 0);
+//        assert.equal(lexAndParseResult.parseErrors.length, 0);
+//        assert.equal(lexAndParseResult.parseErrors.length, 0);
+//        inputText = '5 is not equal to 3';
+//        lexAndParseResult = parseJson(inputText);
+//        lexAndParseResult = parseJson(inputText);
+//        assert.equal(lexAndParseResult.lexErrors.length, 0);
+//        assert.equal(lexAndParseResult.parseErrors.length, 0);
+//        assert.equal(lexAndParseResult.parseErrors.length, 0);
+//       inputText = "'efrat' is equal to 'Daphna'";
+//       inputText = "'efrat' is equal to 'Daphna'";
+//        //inputText = "name of the customer is equal to 'Daphna'";
+//        lexAndParseResult = parseJson(inputText);
+//        lexAndParseResult = parseJson(inputText);
+//        lexAndParseResult = parseJson(inputText);
+//        assert.equal(lexAndParseResult.lexErrors.length, 0);
+//        assert.equal(lexAndParseResult.parseErrors.length, 0);
+        // terms
+        //var inputText = 'age of the player is not equal to \'efrat\'';
+        var inputText = 'age of the player is not equal to 30';
+        var lexAndParseResult = parseJson(inputText);
+
+        assert.equal(lexAndParseResult.lexErrors.length, 0);
+        assert.equal(lexAndParseResult.parseErrors.length, 0);
+
+
+        /*(inputText = "\'efrat\' is equal to \'Daphna\'";
+        lexAndParseResult = parseJson(inputText);
+
+        assert.equal(lexAndParseResult.lexErrors.length, 0);
+        assert.equal(lexAndParseResult.parseErrors.length, 0);*/
+
+        // parser error
+        /*inputText = "'efrat' is equal to 3";
+        lexAndParseResult = parseJson(inputText);
+
+        assert.equal(lexAndParseResult.lexErrors.length, 0);
+        assert.equal(lexAndParseResult.parseErrors.length, 0);
+
+        // lexer error
+        inputText = '5 is equall to 3';
+        lexAndParseResult = parseJson(inputText);
+
+        assert.equal(lexAndParseResult.lexErrors.length, 0);
+        assert.equal(lexAndParseResult.parseErrors.length, 0);*/
+    });
+
+    /*it('can parse a simple Json without errors - Parser implemented using ES6 syntax', function() {
+        // only load a file containing ES6 syntax when actually running the test
+        // thus if this test is ignored the other tests can still be run in old node.js versions
+        var parseJsonES6 = require("./jsonES6");
+        var inputText = '{ "arr": [1,null,true], "obj": {"num":666}}';
+        var lexAndParseResult = parseJsonES6(inputText);
+
+        assert.equal(lexAndParseResult.lexErrors.length, 0);
+        assert.equal(lexAndParseResult.parseErrors.length, 0);
+    });*/
+});
